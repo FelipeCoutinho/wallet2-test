8d7a14fc998be18a2f211b44b2fb30a0a3ecf86d
Your Name <felipe.coutinho@grupoboticario.com.br>
2023-04-02 12:20:40 -0300

    feat: teste de conexão com o postgres

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -11,10 +11,14 @@ import { WalletService } from './wallet.service';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { UpdateWalletDto, paymentDTO } from './dto/update-wallet.dto';
 import { get } from 'http';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('wallet')
 export class WalletController {
-  constructor(private readonly walletService: WalletService) {}
+  constructor(
+    private readonly walletService: WalletService,
+    private readonly prisma: PrismaService
+    ) {}
 
   @Post('/deposit')
   deposit(@Body() bady: any) {
@@ -34,5 +38,10 @@ export class WalletController {
   @Post('chargeback')
   chargeback(@Body() body: paymentDTO) {
     return this.walletService.chargeback(Number(body.value));
+  }
+
+  @Get('/user')
+  findOne() {
+    return this.prisma.user.findFirst( );
   }
 }
